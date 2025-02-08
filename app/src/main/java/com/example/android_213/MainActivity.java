1cfbecf6454b5bac4dfee0c3021f6d7764336417
Babchinsky <babchinsky.prog@gmail.com>
2025-02-08 18:21:08 +0200

    Homework 1

--- app/src/main/java/com/example/android_213/MainActivity.java
+++ app/src/main/java/com/example/android_213/MainActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+//import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//    private TextView tv1;
+    //    private TextView tv1;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +25,29 @@ public class MainActivity extends AppCompatActivity {
 //            return insets;
 //        });
 
+        tv2 = findViewById(R.id.textView2);
+        findViewById(R.id.buttonPlus).setOnClickListener(this::onButtonPlusClick);
+        findViewById(R.id.buttonMinus).setOnClickListener(this::onButtonMinusClick);
 //        tv1 = findViewById(R.id.textView1);
 //        findViewById(R.id.button1).setOnClickListener(this::onButton1Click);
     }
 
-    private void onButton1Click(View view) {
+//    private void onButton1Click(View view) {
+//
+////        String txt = tv1.getText().toString();
+////        txt += "!";
+////        tv1.setText(txt);
+//    }
 
-//        String txt = tv1.getText().toString();
-//        txt += "!";
-//        tv1.setText(txt);
+    private void onButtonPlusClick(View view) {
+        int number = Integer.parseInt(tv2.getText().toString());
+        number += 1;
+        tv2.setText(String.valueOf(number));
+    }
+
+    private void onButtonMinusClick(View view) {
+        int number = Integer.parseInt(tv2.getText().toString());
+        number -= 1;
+        tv2.setText(String.valueOf(number));
     }
 }
