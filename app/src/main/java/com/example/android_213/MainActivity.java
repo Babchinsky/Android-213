058b9511b09ef2ce2e10ad1d6c5e841150ed768d
Babchinsky <babchinsky.prog@gmail.com>
2025-02-08 18:03:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android_213/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android_213;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+//    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+//        tv1 = findViewById(R.id.textView1);
+//        findViewById(R.id.button1).setOnClickListener(this::onButton1Click);
+    }
+
+    private void onButton1Click(View view) {
+
+//        String txt = tv1.getText().toString();
+//        txt += "!";
+//        tv1.setText(txt);
+    }
+}
