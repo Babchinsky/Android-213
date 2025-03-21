0e8fd27ec0907d0dcbc3e66deff0295a579551f5
Babchinsky <babchinsky.prog@gmail.com>
2025-03-21 11:39:40 +0200

    Lesson. Chat

--- app/src/main/java/com/example/android_213/RateActivity.java
+++ app/src/main/java/com/example/android_213/RateActivity.java
@@ -1,5 +1,7 @@
 package com.example.android_213;
 
+import static com.example.android_213.Services.fetchUrlText;
+
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
@@ -197,32 +199,6 @@ public class RateActivity extends AppCompatActivity {
                 .show();
     }
 
-    private String fetchUrlText(String href) throws RuntimeException {
-        if (cache.containsKey(href)) {
-            CacheItem cacheItem = cache.get(href);
-            if (cacheItem != null) {
-                return cacheItem.text;
-            }
-        }
-        try (InputStream urlStream = new URL(href).openStream()) {
-            String text = readAllText(urlStream);
-            cache.put(href, new CacheItem(href, text, null));
-            return text;
-        } catch (IOException ex) {
-            Log.d("loadRates", "Ошибка загрузки: " + ex.getMessage());
-            throw new RuntimeException(ex);
-        }
-    }
-
-    private String readAllText(InputStream inputStream) throws IOException {
-        byte[] buffer = new byte[4096];
-        ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream();
-        int receivedBytes;
-        while ((receivedBytes = inputStream.read(buffer)) > 0) {
-            byteBuilder.write(buffer, 0, receivedBytes);
-        }
-        return byteBuilder.toString();
-    }
 
     private String formatDateForApi(String date) {
         String[] parts = date.split("\\.");
@@ -307,18 +283,4 @@ public class RateActivity extends AppCompatActivity {
         handler.removeMessages(0);
         super.onDestroy();
     }
-
-    static class CacheItem {
-        private String href;
-        private String text;
-        private Date expires;
-
-        public CacheItem(String href, String text, Date expires) {
-            this.href = href;
-            this.text = text;
-            this.expires = expires;
-        }
-    }
-
-    private final Map<String, CacheItem> cache = new HashMap<>();
 }
