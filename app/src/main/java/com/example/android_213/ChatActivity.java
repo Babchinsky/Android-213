b77e029dd2cc66f2fe3b5b0b7056d3d7953ab858
Babchinsky <babchinsky.prog@gmail.com>
2025-03-27 11:39:36 +0200

    Lesson. Chat

--- app/src/main/java/com/example/android_213/ChatActivity.java
+++ app/src/main/java/com/example/android_213/ChatActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -21,14 +22,25 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ChatActivity extends AppCompatActivity {
     private final String chatUrl = "https://chat.momentfor.fun/";
     private final List<ChatMessage> chatMessages = new ArrayList<>();
+    private RecyclerView rvContainer;
     ChatMessageAdapter chatMessageAdapter;
     private ExecutorService pool;
     private EditText etAuthor;
@@ -39,18 +51,29 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pool = Executors.newFixedThreadPool(3);
-        pool.submit(this::loadChat);
-//        EdgeToEdge.enable(this);
+        requestChat();
+
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-        RecyclerView rvContainer = findViewById(R.id.chat_rv_container);
-        rvContainer.setLayoutManager(new LinearLayoutManager(this));
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets imeBars = insets.getInsets(WindowInsetsCompat.Type.ime());
+            v.setPadding(
+                    Math.max(systemBars.left, imeBars.left),
+                    Math.max(systemBars.top, imeBars.top),
+                    Math.max(systemBars.right, imeBars.right),
+                    Math.max(systemBars.bottom, imeBars.bottom)
+            );
+            return insets;
+        });
+
+        rvContainer = findViewById(R.id.chat_rv_container);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        rvContainer.setLayoutManager(linearLayoutManager);
         chatMessageAdapter = new ChatMessageAdapter(chatMessages);
         rvContainer.setAdapter(chatMessageAdapter);
+
         findViewById(R.id.chat_btn_send).setOnClickListener(this::onSendClick);
         etAuthor = findViewById(R.id.chat_et_author);
         etMessage = findViewById(R.id.chat_et_message);
@@ -59,36 +82,80 @@ public class ChatActivity extends AppCompatActivity {
 
     private void onSendClick(View view) {
         String author = etAuthor.getText().toString();
-        if(author.isBlank()){
+        if (author.isBlank()) {
             Toast.makeText(this, R.string.chat_msg_empty_author, Toast.LENGTH_SHORT).show();
             return;
         }
 
         String message = etMessage.getText().toString();
-        if(message.isBlank()){
+        if (message.isBlank()) {
             Toast.makeText(this, R.string.chat_msg_empty_message, Toast.LENGTH_SHORT).show();
             return;
         }
+        CompletableFuture
+                .runAsync(() -> sendMessage(new ChatMessage(author, message)), pool)
+                .thenRun(this::requestChat);
     }
 
-    private void loadChat() {
+    private void requestChat(){
+        CompletableFuture
+                .supplyAsync(this::loadChat, pool)
+                .thenAccept(this::updateChatView);
+    }
+
+    private void sendMessage(ChatMessage chatMessage) {
+        /*
+        Бэк чата принимает сообщение как от формы
+        POST /
+        Content-Type: application/x-www-form-urlencoded
+
+        author=Author&msg=Message
+        */
+        Map<String, String> data = new HashMap<>();
+        data.put("author", chatMessage.getAuthor());
+        data.put("msg", chatMessage.getText());
+        if ( Services.postForm(chatUrl, data)){
+            runOnUiThread(() -> etMessage.setText(""));
+        }
+        else {
+            Toast.makeText(ChatActivity.this, "Ошибка. Повторите позже", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private int loadChat() {
         try {
+            int oldSize = chatMessages.size();
             String text = Services.fetchUrlText(chatUrl);
             JSONObject jsonObject = new JSONObject(text);
             JSONArray arr = jsonObject.getJSONArray("data");
             int len = arr.length();
             for (int i = 0; i < len; i++) {
-                chatMessages.add(
-                        ChatMessage.fromJsonObject(
-                                arr.getJSONObject(i)
-                        )
-                );
+                ChatMessage chatMessage = ChatMessage.fromJsonObject(arr.getJSONObject(i));
+                if (chatMessages.stream().noneMatch(m -> m.getId().equals(chatMessage.getId()))) {
+                    chatMessages.add(chatMessage);
+                }
             }
-            runOnUiThread(() -> chatMessageAdapter.notifyDataSetChanged());
-//            runOnUiThread(() -> chatMessageAdapter.notifyItemRangeInserted(0, len));
-        } catch (RuntimeException | JSONException ex) {
-            Log.d("loadChat", ex.getCause() + ex.getMessage());
+            int newSize = chatMessages.size();
+            if (newSize > oldSize) {
+                chatMessages.sort(Comparator.comparing(ChatMessage::getMoment));
+            }
+            return newSize - oldSize;
         }
+        catch (RuntimeException | JSONException ex) {
+            Log.d("loadChat", ex.getCause() + ex.getMessage());
+            return 0;
+        }
+    }
+
+    private void updateChatView(int newMessagesCount) {
+        if (newMessagesCount <= 0) {
+            return;
+        }
+        int size = chatMessages.size();
+        runOnUiThread(() -> {
+            chatMessageAdapter.notifyItemRangeChanged(size - newMessagesCount, newMessagesCount);
+            rvContainer.scrollToPosition(size - 1); // -1 -- index from 0
+        });
     }
 
     @Override
--- app/src/main/java/com/example/android_213/chat/ChatMessageViewHolder.java
+++ app/src/main/java/com/example/android_213/chat/ChatMessageViewHolder.java
@@ -1,28 +1,46 @@
 package com.example.android_213.chat;
 
 import com.example.android_213.R;
+import com.example.android_213.orm.ChatMessage;
+
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class ChatMessageViewHolder extends RecyclerView.ViewHolder {
+    public static final SimpleDateFormat dateFormat =
+            new SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.ROOT);
+
     private TextView tvAuthor;
     private TextView tvText;
+    private TextView tvMoment;
+    private ChatMessage chatMessage;
 
     public ChatMessageViewHolder(@NonNull View itemView){
         super(itemView);
         tvAuthor = itemView.findViewById(R.id.chat_msg_author);
         tvText = itemView.findViewById(R.id.chat_msg_text);
+        tvMoment = itemView.findViewById(R.id.chat_msg_moment);
     }
 
-    public TextView getTvAuthor() {
-        return tvAuthor;
+    public void setChatMessage(ChatMessage chatMessage) {
+        this.chatMessage = chatMessage;
+        showMessage();
     }
 
-    public TextView getTvText() {
-        return tvText;
+    public void showMessage(){
+        tvAuthor.setText(chatMessage.getAuthor());
+        tvText.setText(chatMessage.getText());
+        /*
+        TODO: не выводить дату(только время), если дата - сегодня
+         заменять дату на "вчера" или день назад, а такжюже "2 дня назад" и так далее
+        */
+        tvMoment.setText(dateFormat.format(this.chatMessage.getMoment()));
     }
 }
 /*
